5e019c14c3146b52abc5cf4eb80a45acac963d33
Jaime Radulovich <jradlov@gmail.com>
2018-06-29 11:34:28 -0700

    Add project files.

---
+++ MasterDetail_3/MasterDetail_3/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetail_3
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        private List<MyMenuItem> _menuItems;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _menuItems = new List<MyMenuItem> {
+                new MyMenuItem {Name="Page1", Image="robot_purple.png"},
+                new MyMenuItem {Name="Page2", Image="robot_gray.png"}
+            };
+
+            // each MyMenuItem in _menuItems is the Binding Context
+            // for its corresponding item in ItemsSource
+            masterListView.ItemsSource = _menuItems; 
+
+
+            // first thing shown is a Detail Page.  Set this page here
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;  // present master page?
+        }
+
+        private void masterListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // without this check the app crashes - why ???
+            if (e.SelectedItem == null) return;
+
+            var menuChoice = e.SelectedItem as MyMenuItem;
+            //DisplayAlert("menuChoice", menuChoice.Name, "Ok");
+
+            string str = menuChoice.Name;
+
+            if (str == _menuItems[0].Name)
+                Detail = new NavigationPage(new Page1());
+            else if (str == _menuItems[1].Name)
+                Detail = new NavigationPage(new Page2());
+
+            IsPresented = false;  // present master page?
+
+            // To deselect the item (otherwise it's highlighted when clicked on)
+            masterListView.SelectedItem = null;
+        }
+    }
+}
---
+++ MasterDetail_3/MasterDetail_3/MyMenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterDetail_3
+{
+    public class MyMenuItem
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
